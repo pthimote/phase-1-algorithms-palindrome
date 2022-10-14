d23fcffec56c812ebc8023db51ef1cbf7d17d9a1
Paul Thimote <pthimote@gmail.com>
2022-10-14 19:25:45 -0400

    commit tree 2c88b5b517e1aac8dd0d1d8e597f9d9fed677d25

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function isPalindrome(word) {
   // Write your algorithm here
+let reversedWord = word.split('').reverse().join('')
+return (reversedWord === word)
 }
 
 /* 
@@ -16,7 +18,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
